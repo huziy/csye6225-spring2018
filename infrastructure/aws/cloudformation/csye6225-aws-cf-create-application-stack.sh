a77810bc18d6691d2e5cfc311337f17f09499d3e
huziy <hu.ziy@husky.neu.edu>
2018-02-15 17:32:00 -0500

    assignment5

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -6,3 +6,6 @@ read stackname
 instanceName="$stackname-csye6225-instance"
 
 aws cloudformation create-stack --template-body file://./csye6225-cf-application.json --stack-name ${stackname} --parameters ParameterKey=InstanceName,ParameterValue=$instanceName
+
+aws cloudformation wait stack-create-complete --stack-name ${stackname}
+done
