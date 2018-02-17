9388d3e5d13eaf66186f558711ca622869030f47
davidcruise <liujunyumail@gmail.com>
2018-02-17 11:24:06 -0500

    travis

--- webapp/src/main/java/com/csye6225/spring2018/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.csye6225.spring2018.repository;
-
-
-import com.csye6225.spring2018.entity.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User,Long> {
-
-}
