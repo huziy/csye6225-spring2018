219fdbff95289f30abed34ab11066eb6344e025e
yuan <huhudeni@gmail.com>
2018-02-23 23:24:27 -0500

    lab

--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-application-stack.sh
@@ -4,4 +4,6 @@ echo "Enter your stack name"
 read stackname
 
 aws cloudformation delete-stack --stack-name ${stackname}&&
+
+aws cloudformation wait stack-delete-complete --stack-name ${stackname}
 echo done
