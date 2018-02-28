f80b819278557fa496145acc4c79e57f5db3022f
huziy <hu.ziy@northeastern.edu>
2018-02-28 17:25:24 -0500

    Update csye6225-aws-cf-create-ci-cd-stack.sh

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-ci-cd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-ci-cd-stack.sh
@@ -19,5 +19,6 @@ echo $SUBNET_ID4&&
 echo "Enter The Stack Name:"
 read stackname
 
-aws cloudformation create-stack --stack-name ${stackname} --template-body file://./template.json --parameters ParameterKey=ParamVpcID,ParameterValue=${VPC_ID} ParameterKey=ParamSubnetId1,ParameterValue=${SUBNET_ID1} ParameterKey=ParamSubnetId2,ParameterValue=${SUBNET_ID2} ParameterKey=ParamSubnetId3,ParameterValue=${SUBNET_ID3} ParameterKey=ParamSubnetId4,ParameterValue=${SUBNET_ID4} --capabilities CAPABILITY_NAMED_IAM&&
+aws cloudformation create-stack --stack-name ${stackname} --template-body file://./csye6225-cf-ci-cd.json --parameters ParameterKey=ParamVpcID,ParameterValue=${VPC_ID} ParameterKey=ParamSubnetId1,ParameterValue=${SUBNET_ID1} ParameterKey=ParamSubnetId2,ParameterValue=${SUBNET_ID2} ParameterKey=ParamSubnetId3,ParameterValue=${SUBNET_ID3} ParameterKey=ParamSubnetId4,ParameterValue=${SUBNET_ID4} --capabilities CAPABILITY_NAMED_IAM&&
+
 echo done
