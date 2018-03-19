48c426d4aafe0216e011965aa2c2ae9cf69b2c13
huziy <hu.ziy@northeastern.edu>
2018-03-18 21:10:08 -0400

    Update ApplicationConstant.java

--- webapp/src/main/java/com/csye6225/spring2018/constant/ApplicationConstant.java
+++ webapp/src/main/java/com/csye6225/spring2018/constant/ApplicationConstant.java
@@ -2,8 +2,8 @@ package com.csye6225.spring2018.constant;
 
 public class ApplicationConstant {
 
-    public static final String extractPrefixPath = "/hu.ziy/Downloads/";
-    public static final String savePrefixPath = "/hu.ziy/Downloads";
+    public static final String extractPrefixPath = "/home/huziy/Downloads/";
+    public static final String savePrefixPath = "/home/huziy/Downloads";
     public static final String applicationPrefixPath = "/images/";
 
     public static final String bucketName = "s3.csye6225-spring2018-huziy.me";
