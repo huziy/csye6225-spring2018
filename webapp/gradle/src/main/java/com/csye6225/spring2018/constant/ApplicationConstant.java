1992a6785a9c0017a9e8286c5b38c7e8dabeb874
yuan <huhudeni@gmail.com>
2018-03-19 15:06:14 -0400

    merge Ziyan master

--- webapp/gradle/src/main/java/com/csye6225/spring2018/constant/ApplicationConstant.java
+++ webapp/gradle/src/main/java/com/csye6225/spring2018/constant/ApplicationConstant.java
@@ -2,9 +2,9 @@ package com.csye6225.spring2018.constant;
 
 public class ApplicationConstant {
 
-    public static final String extractPrefixPath = "/home/liy/Pictures/csye6225-spring2018-master/webapp/src/main/resources/images/";
+    public static final String extractPrefixPath = "/home/huziy/Downloads/";
     public static final String savePrefixPath = "/home/huziy/Downloads";
     public static final String applicationPrefixPath = "/images/";
 
-    public static final String bucketName = "s3.csye6225-spring2018-liyuan.me";
+    public static final String bucketName = "s3.csye6225-spring2018-huziy.me";
 }
