a8dfce1fca0b8bb8cb7122d22ffb163d7413fedd
huziy <hu.ziy@northeastern.edu>
2018-03-29 01:52:40 -0400

    Update ApplicationConstant.java

--- webapp/src/main/java/com/csye6225/spring2018/constant/ApplicationConstant.java
+++ webapp/src/main/java/com/csye6225/spring2018/constant/ApplicationConstant.java
@@ -2,9 +2,4 @@ package com.csye6225.spring2018.constant;
 
 public class ApplicationConstant {
     public static final String bucket= "s3.csye6225-spring2018-huziy.me";
-
-<<<<<<< HEAD
-    public static final String bucket = "s3.csye6225-spring2018-huziy.me";
-=======
->>>>>>> origin/master
 }
